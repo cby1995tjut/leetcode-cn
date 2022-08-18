05c46d597d39e2e5077924485f0f02d4e52862f5
davon <747664918@qq.com>
2022-08-18 22:01:21 +0800

    LengthOfLongestSubstring for tomorrow

---
+++ src/main/java/middle/LengthOfLongestSubstring.java
@@ -0,0 +1,14 @@
+package middle;
+
+//给定一个字符串 s 请你找出其中不含有重复字符的最长子串的长度
+//
+//输入: s = "abcabcbb"
+//输出: 3
+//解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3
+
+public class LengthOfLongestSubstring {
+
+    public static void main(String[] args) {
+
+    }
+}
